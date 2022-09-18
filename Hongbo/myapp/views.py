45abbec443fd6d32570b8d1e8d1fd7691217ef1c
eusang <dbtkd98@naver.com>
2022-09-18 17:56:26 +0900

    이미지 링크 수정

--- Hongbo/myapp/views.py
+++ Hongbo/myapp/views.py
@@ -8,8 +8,8 @@ def main(request):
 
 
     data = pd.read_csv("https://raw.githubusercontent.com/3307rla/Hongbo/main/Hongbo/myapp/static/csv/%ED%8C%90%EC%B4%89%EB%AC%BC%EC%97%85%EC%B2%B4.csv", encoding='euc-kr')
-    ndata = data[['LABEL-1', 'LABEL-3', 'site']]
-    ndata.columns = ['이름', '주소', '사이트']
+    ndata = data[['LABEL-1', 'LABEL-3', 'site', 'img']]
+    ndata.columns = ['이름', '주소', '사이트', '이미지']
     # print(ndata.iloc[[0,1,3,5]])
     li = []
     for i in range(9):
@@ -26,7 +26,8 @@ def main(request):
         dict['dict'+str(i)] = {
             '이름':df.iloc[i].이름,
             '주소':df.iloc[i].주소,
-            '사이트':df.iloc[i].사이트
+            '사이트':df.iloc[i].사이트,
+            '이미지':df.iloc[i].이미지
             }
     
     return render(request, 'main.html', {'dict':dict})
