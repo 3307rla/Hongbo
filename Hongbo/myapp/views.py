3f4bfb04ab9788f7d8944ef4b3eea180995fee3c
woohyeok <woohyeok@DESKTOP-KQ4165E>
2022-09-21 12:12:42 +0900

    sss

--- Hongbo/myapp/views.py
+++ Hongbo/myapp/views.py
@@ -58,9 +58,6 @@ def mapFunc(request):
         
     return render(request, "hmap.html", {'x1':x1, 'x2':x2, 'x3':x3, 'y1':y1, 'y2':y2, 'y3':y3})
 
-def mapFunc(request):
-        
-    return render(request)
         
 def statistics(request):
     
